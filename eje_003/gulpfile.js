a5e0719fe54c52bc305f7d2a7e21a68ba44a2192
David Rivero <davidmriverog@gmail.com>
2016-04-15 21:23:01 -0430

    ejercicio nro. 3, ejemplo de como instalar plugin

--- eje_003/gulpfile.js
+++ eje_003/gulpfile.js
@@ -1,7 +1,8 @@
 /*
 * Dependencias generales
 */
-var gulp     =  require('gulp');
+var gulp     =  require('gulp'),
+    uglify   =  require('uglify');
 
 gulp.task('saludar',function(){
   console.log("Hola este es mi primera tarea");
