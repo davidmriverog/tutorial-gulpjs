d66fcb6be07986ae6f9990053fdfb6c8d50bbb56
David Rivero <davidmriverog@gmail.com>
2016-04-15 21:25:39 -0430

    inicio del ejercicio nro. 4

---
+++ eje_004/gulpfile.js
@@ -0,0 +1,9 @@
+/*
+* Dependencias generales
+*/
+var gulp     =  require('gulp'),
+    uglify   =  require('uglify');
+
+gulp.task('saludar',function(){
+  console.log("Hola este es mi primera tarea");
+});
