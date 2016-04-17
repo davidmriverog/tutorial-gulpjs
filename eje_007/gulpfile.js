69e255364353b94f769d3a1b82f648b04f13d6ab
David M. Rivero G <davidmriverog@gmail.com>
2016-04-16 20:34:01 -0430

    ejercicio nro. 7, tareas multiples por defecto

--- eje_007/gulpfile.js
+++ eje_007/gulpfile.js
@@ -1,16 +1,29 @@
 /*
 * Dependencias generales
 */
-var gulp     =  require('gulp'),
-    uglify   =  require('gulp-uglify'),
-    concat   =  require('gulp-concat'),
-	cssmin   = require('gulp-cssmin'),
-    rename   = require('gulp-rename');
+var gulp       =  require('gulp'),
+    cssmin     =  require('gulp-cssmin'),
+    uglify     =  require('gulp-uglify'),
+    concat     =  require('gulp-concat'),
+	concatCss  =  require('gulp-concat-css'),
+	rename     =  require('gulp-rename');
 	
 // Inicializador de tareas 
+
+gulp.task('cssconcat',function(){
+ return gulp.src('src/css/**/*.css')
+    .pipe(concatCss("theme.css"))
+	.pipe(cssmin())
+	.pipe(rename({suffix:'.min'}))
+    .pipe(gulp.dest('dist/css'));
+});
+
 gulp.task('jsconcat',function(){
   return gulp.src('src/js/**/*.js')
     .pipe(concat('operation.min.js'))
 	.pipe(uglify())
 	.pipe(gulp.dest('dist/js'));
 });
+
+// iniciamos nuestro gestor automatico de tareas multiples.
+gulp.task('default',['cssconcat','jsconcat']);
